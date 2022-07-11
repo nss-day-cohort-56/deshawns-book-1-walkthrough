3c0c921b079466c96b95cb1a16bcf674dfdcedf6
hannahhall <hannahkhall13@gmail.com>
2022-07-11 15:06:59 -0500

    ch 8

---
+++ initial_data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `walker` (
+    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `name`    TEXT NOT NULL,
+    `email`    TEXT NOT NULL
+);
+
+INSERT INTO `walker` VALUES (null, "Larry Fine", "larry@aol.com");
+INSERT INTO `walker` VALUES (null, "Curly Howard", "curly@aol.com");
+INSERT INTO `walker` VALUES (null, "Moe Howard", "moe@aol.com");
+
+-- TODO: create the dog table and 3 insert statements
